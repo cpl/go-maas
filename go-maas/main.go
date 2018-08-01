581c5ce8d697dc98b0ce47279b7b50d0da2214ec
Alexandru-Paul Copil (thee-engineer) <alexandru.p.copil@gmail.com>
2018-08-01 11:05:25 +0100

    Add basic http server

--- go-maas/main.go
+++ go-maas/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 const apiURL = "http://cab.inta-csic.es/rems/wp-content/plugins/marsweather-widget/api.php"
 
-func main() {
+func testRequest() {
 	// GET data from INTA-CSIC API
 	res, err := http.Get(apiURL)
 	if err != nil {
@@ -27,4 +30,15 @@ func main() {
 	for index, sol := range csicResponse.Soles {
 		fmt.Println(index, sol2fsol(sol))
 	}
+}
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", handleIndex)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%q", html.EscapeString(r.URL.Path))
 }
